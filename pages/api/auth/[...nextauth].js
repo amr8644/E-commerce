f09a2ff25bdc00da79bc09100e2f20bf02bb9c5f
amr8644 <amr8644@gmail.com>
2022-06-21 19:08:05 +0300

    fixed issues #3

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,30 +2,9 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/connectDB";
-import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
   providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        try {
-          const client = await clientPromise;
-
-          const user_exist = await client.db().collection("users").findOne({
-            name: credentials?.name,
-          });
-          client.close();
-          return user_exist;
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
