bdc62e6303deeb3bded633c3c85f00d4fbe8c352
amr8644 <amr8644@gmail.com>
2022-06-21 18:39:07 +0300

    fixed issues #!

--- pages/componets/Avatar.js
+++ pages/componets/Avatar.js
@@ -4,10 +4,9 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart, faBell } from "@fortawesome/free-regular-svg-icons";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-// import  Profile from "";
 
 const Avatar = () => {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
 
   return (
     <div className="sm:hidden h-full lg:flex w-1/3  flex items-center justify-end ">
@@ -28,7 +27,7 @@ const Avatar = () => {
         <div class="w-10 h-10 rounded-full ring  ring-orange2 ring-offset-base-100  cursor-pointer mx-3">
           <Image
             className="h-10 w-10 rounded-full"
-            src={session.user.image}
+            src="/Avatar.jpg"
             alt="Avatar"
             width={100}
             height={100}
