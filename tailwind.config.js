6e0c2731151ae3688e0c6a392d6bfb72520e3307
amr8644 <amr8644@gmail.com>
2022-06-21 20:29:05 +0300

    fixed file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   content: [
+    "./sections/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./sections/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
