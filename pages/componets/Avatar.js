e990005bb35553f3ccc16c096060229e4de13cc0
amr8644 <amr8644@gmail.com>
2022-06-22 19:44:21 +0300

    fixed issue #5

--- pages/componets/Avatar.js
+++ pages/componets/Avatar.js
@@ -25,25 +25,14 @@ const Avatar = () => {
 
       <div class="avatar">
         <div class="w-10 h-10 rounded-full ring  ring-orange2 ring-offset-base-100  cursor-pointer mx-3">
-          {session.user ? (
-            <Image
-              className=" h-14 w-14 rounded-full"
-              src={session.user.image}
-              alt="Avatar"
-              width={100}
-              height={100}
-              layout="intrinsic"
-            />
-          ) : (
-            <Image
-              className=" h-14 w-14 rounded-full"
-              src="/Avatar.jpg"
-              alt="Avatar"
-              width={100}
-              height={100}
-              layout="intrinsic"
-            />
-          )}
+          <Image
+            className="h-10 w-10 rounded-full"
+            src={`${session ? session.user.image : "/Avatar.jpg"}`}
+            alt="Avatar"
+            width={100}
+            height={100}
+            layout="intrinsic"
+          />
         </div>
       </div>
     </div>
--- pages/componets/Avatar2.js
+++ pages/componets/Avatar2.js
@@ -8,33 +8,18 @@ const Avatar2 = () => {
     <div className="py-5 flex w-full items-center justify-center flex-col ">
       <div class="avatar h-1/2 w-full flex justify-start">
         <div class="w-12 h-12 rounded-full ring  ring-orange2 ring-offset-base-100  cursor-pointer mx-3">
-          {session.user ? (
-            <Image
-              className=" h-14 w-14 rounded-full"
-              src={session.user.image}
-              alt="Avatar"
-              width={100}
-              height={100}
-              layout="intrinsic"
-            />
-          ) : (
-            <Image
-              className=" h-14 w-14 rounded-full"
-              src="/Avatar.jpg"
-              alt="Avatar"
-              width={100}
-              height={100}
-              layout="intrinsic"
-            />
-          )}
+          <Image
+            className="h-10 w-10 rounded-full"
+            src={`${session ? session.user.image : "/Avatar.jpg"}`}
+            alt="Avatar"
+            width={100}
+            height={100}
+            layout="intrinsic"
+          />
         </div>
-        {session.user ? (
-          <h2 className="text-white font-bold text-base mt-3">
-            {session.user.name}
-          </h2>
-        ) : (
-          <h2 className="text-white font-bold text-base mt-3">User</h2>
-        )}
+        <h2 className="text-white font-bold text-base mt-3">
+          {session ? session.user.name : User}
+        </h2>
       </div>
     </div>
   );
