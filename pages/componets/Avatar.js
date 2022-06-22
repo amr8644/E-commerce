a9d450e910afc34608e9e4ceb7a2492c69cd2e6b
amr8644 <amr8644@gmail.com>
2022-06-22 19:27:51 +0300

    fixed issue#2

--- pages/componets/Avatar.js
+++ pages/componets/Avatar.js
@@ -25,14 +25,25 @@ const Avatar = () => {
 
       <div class="avatar">
         <div class="w-10 h-10 rounded-full ring  ring-orange2 ring-offset-base-100  cursor-pointer mx-3">
-          <Image
-            className="h-10 w-10 rounded-full"
-            src={session.user.image}
-            alt="Avatar"
-            width={100}
-            height={100}
-            layout="intrinsic"
-          />
+          {session ? (
+            <Image
+              className=" h-14 w-14 rounded-full"
+              src={session.user.image}
+              alt="Avatar"
+              width={100}
+              height={100}
+              layout="intrinsic"
+            />
+          ) : (
+            <Image
+              className=" h-14 w-14 rounded-full"
+              src="/public/Avatar.jpg"
+              alt="Avatar"
+              width={100}
+              height={100}
+              layout="intrinsic"
+            />
+          )}
         </div>
       </div>
     </div>
