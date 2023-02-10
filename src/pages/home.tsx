22f35dcd5038c333acabb6fed3a95169ad9469a7
amr8644 <amr8644@gmail.com>
2023-02-10 17:46:11 +0300

    Added API url

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { use } from "react";
+import React from "react";
 import Banner from "@/components/Banner";
 import Navigation from "@/components/Navigation";
 import Row from "@/components/Row";
