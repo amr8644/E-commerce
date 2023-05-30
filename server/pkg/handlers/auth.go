1968037de6dc8a2c42539690085dcaee89600c29
amr8644 <amr8644@gmail.com>
2023-05-30 18:08:37 +0300

    New project

---
+++ server/pkg/handlers/auth.go
@@ -0,0 +1,11 @@
+package handlers
+
+
+// import "github.com/server/pkg/models"
+
+func RegisterUser(){
+
+}
+
+
+//FQ03gaf7aQNVLY9xqHiHzg
--- src/pages/api/URL.ts
+++
@@ -1,4 +0,0 @@
-export const API_URL =
-   process.env.NODE_ENV == "production"
-      ? "https://amazon-clone-nine-amber.vercel.app/login"
-      : "http:localhost:3000";
