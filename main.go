e8c9cf6e3a08763067f8ce61cf1ba41cadd1b348
nyx6965 <72759681+nyx6965@users.noreply.github.com>
2024-05-18 11:58:39 +0000

    fix: removed url

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/server/pkg/handlers"
 )
 
-var url string = "https://e-commerce-l6wb.onrender.com"
+var url string = ""
 
 func main() {
 
@@ -33,10 +33,10 @@ func main() {
 	})
 
 	// Static Routes
-	r.Get(url+"/signup", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/signup", func(w http.ResponseWriter, r *http.Request) {
 		templ.ExecuteTemplate(w, "register.html", nil)
 	})
-    r.Get(url+"/signin", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/signin", func(w http.ResponseWriter, r *http.Request) {
 		templ.ExecuteTemplate(w, "login.html", nil)
 	})
 
@@ -54,24 +54,24 @@ func main() {
 		http.ServeFile(w, r, "scripts/auth.js")
 	})
 
-	r.Get(url+"/dashboard", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/dashboard", func(w http.ResponseWriter, r *http.Request) {
 		templ.ExecuteTemplate(w, "dashboard.html", nil)
 	})
 
-	r.Get(url+"/mycart", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/mycart", func(w http.ResponseWriter, r *http.Request) {
 		templ.ExecuteTemplate(w, "viewcart.html", nil)
 	})
 
 	// Auth
-	r.Post(url+"/register", handlers.HTTPHandler(handlers.RegisterUser))
-	r.Get(url+"/logout", handlers.HTTPHandler(handlers.LogoutUser))
-	r.Post(url+"/login", handlers.HTTPHandler(handlers.LoginUser))
+	r.Post("/register", handlers.HTTPHandler(handlers.RegisterUser))
+	r.Get("/logout", handlers.HTTPHandler(handlers.LogoutUser))
+	r.Post("/login", handlers.HTTPHandler(handlers.LoginUser))
 
 	// Items
-	r.Post(url+"/add-item", handlers.HTTPHandler(handlers.AddProduct))
-	r.Delete(url+"/delete/{id}", handlers.HTTPHandler(handlers.DeleteProduct))
-	r.Get(url+"/cart/{id}", handlers.HTTPHandler(handlers.GetAllUserProduct))
-	r.Put(url+"/update", handlers.HTTPHandler(handlers.UpdateProduct))
+	r.Post("/add-item", handlers.HTTPHandler(handlers.AddProduct))
+	r.Delete("/delete/{id}", handlers.HTTPHandler(handlers.DeleteProduct))
+	r.Get("/cart/{id}", handlers.HTTPHandler(handlers.GetAllUserProduct))
+	r.Put("/update", handlers.HTTPHandler(handlers.UpdateProduct))
 
 	http.ListenAndServe(":8000", handlers.Manager.LoadAndSave(r))
 }
