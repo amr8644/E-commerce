0d3b40c2f1e804be5e5e683ea026cb654e4a6c64
nyx6965 <72759681+nyx6965@users.noreply.github.com>
2024-08-16 23:31:50 +0300

    React

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom';
 import { Button } from "@/components/ui/button"
+import MainLayout from "./layout/MainLayout.tsx"
+import HomePage from "./pages/HomePage.tsx"
 
-export default function Home() {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  )
-}
+export default function App() {
+   const router = createBrowserRouter(
+     createRoutesFromElements(
+       <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+       </Route>
+     )
+   );
+
+   return <RouterProvider router={router} />;
+ };
